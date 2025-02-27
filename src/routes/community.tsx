71247e4fcb0ffe651d42933b5d6dc5d9ae1168ed
NataniaReginaCS <141817075+NataniaReginaCS@users.noreply.github.com>
2025-02-27 13:59:08 +0700

    responsive page community

--- src/routes/community.tsx
+++ src/routes/community.tsx
@@ -2,7 +2,17 @@ import * as React from "react";
 import { createFileRoute } from "@tanstack/react-router";
 import wallpaperCommunity from "../assets/community.png";
 import community1 from "../assets/community1.png";
-import { FaWhatsapp, FaDiscord, FaTelegram, FaFacebook, FaMap, FaPhone, FaEnvelope, FaInstagram, FaTiktok } from "react-icons/fa6";
+import {
+	FaWhatsapp,
+	FaDiscord,
+	FaTelegram,
+	FaFacebook,
+	FaMap,
+	FaPhone,
+	FaEnvelope,
+	FaInstagram,
+	FaTiktok,
+} from "react-icons/fa6";
 import wallpaperAbout from "../assets/about.png";
 import iconAbout from "../assets/aboutIcon.png";
 
@@ -24,13 +34,18 @@ function Community() {
 					Join the ReLeaf Fashion Community
 				</h1>
 				<p className="text-[#142E38] text-lg md:text-2xl max-w-5xl z-10">
-					Connect, share, and grow together in a community that cares about sustainable fashion.
+					Connect, share, and grow together in a community that cares about
+					sustainable fashion.
 				</p>
 				<p className="text-[#142E38] text-lg md:text-2xl max-w-2xl z-10">
-					Find inspiration, discussions, and exciting challenges to make preloved fashion a lifestyle.
+					Find inspiration, discussions, and exciting challenges to make
+					preloved fashion a lifestyle.
 				</p>
 				<div className="mt-10 flex flex-col md:flex-row gap-5 z-10 text-md md:text-lg ">
-					<button className="bg-white outline outline-[#D5EEC6] w-45 text-[#333333] px-6 py-2 rounded-full hover:bg-green-100" onClick={() => (window.location.href = "#startLearning")}>
+					<button
+						className="bg-white outline outline-[#D5EEC6] w-45 text-[#333333] px-6 py-2 rounded-full hover:bg-green-100"
+						onClick={() => (window.location.href = "#startLearning")}
+					>
 						About Us
 					</button>
 					<button className="bg-[#FFD8BE] text-[#333333] px-6 py-2 rounded-full hover:bg-green-100">
@@ -42,7 +57,7 @@ function Community() {
 				</div>
 			</section>
 
-			<section className="pt-50 md:pt-70 bg-white">
+			<section className="pt-10 md:pt-70 bg-white">
 				<div className="grid grid-cols-1 md:grid-cols-2 md:ml-30 items-center text-center md:text-left">
 					<h1 className="text-[#142E38] text-3xl md:text-5xl font-bold mx-15 md:mx-0">
 						ReLeaf Community: Connect, Share, and Make an Impact
@@ -53,14 +68,19 @@ function Community() {
 						className="w-2/5 md:w-3/5 pt-10 md:pt-0 md:row-span-2 mx-auto md:self-end"
 					/>
 					<p className="text-[#142E38] text-lg md:text-xl text-justify mt-5 mx-15 md:mx-0 md:mt-10">
-						ReLeaf Fashion Community is a space for those who care about sustainable fashion. Here, you can share information about clothing donations, preloved fashion, and various community actions that support an eco-friendly lifestyle. Join to stay connected, contribute, and inspire positive change together!
+						ReLeaf Fashion Community is a space for those who care about
+						sustainable fashion. Here, you can share information about clothing
+						donations, preloved fashion, and various community actions that
+						support an eco-friendly lifestyle. Join to stay connected,
+						contribute, and inspire positive change together!
 					</p>
 				</div>
 			</section>
 
-
 			<section className="bg-white pt-20 pb-30 md:pt-40 text-[#142E38] text-center">
-				<h1 className="text-3xl md:text-5xl font-bold mt-10 mb:mt-10">Join Us</h1>
+				<h1 className="text-3xl md:text-5xl font-bold mt-10 mb:mt-10">
+					Join Us
+				</h1>
 				<p className="text-[#142E38] mt-5 md:text-xl md:mt-10  ">
 					Be Part of the Change! Connect, share, and support sustainable fashion
 					with ReLeaf Fashion.
@@ -104,72 +124,90 @@ function Community() {
 
 			<section className="relative bg-white text-[#333333]">
 				<div className="absolute inset-0">
-					<img className="w-full h-full object-cover opacity-60" src={wallpaperAbout} alt="wallpaperAbout" />
+					<img
+						className="w-full h-full object-cover opacity-60"
+						src={wallpaperAbout}
+						alt="wallpaperAbout"
+					/>
 					<div className="absolute inset-0 h-full bg-[#CFCFCF] opacity-70"></div>
 				</div>
 				<div className="relative grid grid-cols-1 md:grid-cols-3 mx-5 md:mx-20 py-10 items-center">
 					<div className="flex flex-col text-center md:text-left  md:col-span-2 md:py-10">
 						<h1 className="text-3xl md:text-5xl font-bold z-10">About Us</h1>
 						<p className="mt-5 text-lg text-justify z-10 md:py-4">
-							ReLeaf Fashion is a community that believes in the power of sustainable fashion. We are committed to promoting eco-friendly practices and supporting a circular economy. Our mission is to inspire positive change in the fashion industry by encouraging people to embrace preloved fashion and reduce waste. Join us in our journey to create a greener future for all!
+							ReLeaf Fashion is a community that believes in the power of
+							sustainable fashion. We are committed to promoting eco-friendly
+							practices and supporting a circular economy. Our mission is to
+							inspire positive change in the fashion industry by encouraging
+							people to embrace preloved fashion and reduce waste. Join us in
+							our journey to create a greener future for all!
 						</p>
 						<div className="flex flex-col gap-3 mt-5 z-10">
 							<p className="text-md bg-[#D5EEC6] rounded-full px-5 py-2 z-10 min-w-0 text-center md:text-left">
-								Hanger melambangkan fashion, sementara daun mewakili keberlanjutan dan eco-friendly.
+								Hanger melambangkan fashion, sementara daun mewakili
+								keberlanjutan dan eco-friendly.
 							</p>
 							<p className="text-md bg-[#D5EEC6] rounded-full px-5 py-2 z-10 min-w-0 text-center md:text-left">
-								Hijau lembut dan warna earthy mencerminkan alam, kesegaran, dan etika bisnis ramah lingkungan.
+								Hijau lembut dan warna earthy mencerminkan alam, kesegaran, dan
+								etika bisnis ramah lingkungan.
 							</p>
 							<p className="text-md bg-[#D5EEC6] rounded-full px-5 py-2 z-10 min-w-0 text-center md:text-left">
-								Minimalis dan modern, mencerminkan transparansi, profesionalisme, dan produksi bertanggung jawab.
+								Minimalis dan modern, mencerminkan transparansi,
+								profesionalisme, dan produksi bertanggung jawab.
 							</p>
 						</div>
 					</div>
 					<img
 						src={iconAbout}
 						alt="iconAbout"
-						className="mt-10 md:mt-0 flex justify-center w-3/5 md:w-full object-cover h-80 mx-auto md:ml-18 z-10"
+						className="mt-10 md:mt-0 flex justify-center w-3/5 md:w-100 object-cover h-80 mx-auto md:ml-18 z-10"
 					/>
 				</div>
 			</section>
 
 			<section className="bg-white text-[#333333] pt-20 text-center pb-20">
-				<h1 className="text-3xl md:text-5xl font-bold mt-10 mb:mt-10 ">Contact Us</h1>
+				<h1 className="text-3xl md:text-5xl font-bold mt-10 mb:mt-10 ">
+					Contact Us
+				</h1>
 				<p className="text-[#142E38] mt-5 md:text-xl md:mt-10  ">
-				Be Part of the Change! Connect, share, and support sustainable fashion with ReLeaf Fashion.
+					Be Part of the Change! Connect, share, and support sustainable fashion
+					with ReLeaf Fashion.
 				</p>
 				<div className="mt-10 flex flex-col md:flex-row justify-center items-center gap-2">
-				<iframe
-					className="w-full h-60 mb-4"
-					src="https://maps.google.com/maps?width=100%25&amp;height=800&amp;hl=en&amp;q=Universitas%20Atma%20Jaya%20Yogyakarta%20Kampus%203&amp;t=&amp;z=19&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-					allowFullScreen
-				></iframe> 
-				<div className="text-left">
-					<div className="flex flex-row space-x-4 mx-4 pb-4">
-					<FaMap className="md:w-11 w-10 md:h-11 h-10" />
-					<p>Jl. Babarsari No.43, Janti, Caturtunggal, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281</p>
+					<iframe
+						className="w-full h-60 mb-4 md:mx-10 md:w-1/2 md:h-96"
+						src="https://maps.google.com/maps?width=100%25&amp;height=800&amp;hl=en&amp;q=Universitas%20Atma%20Jaya%20Yogyakarta%20Kampus%203&amp;t=&amp;z=19&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+						allowFullScreen
+					></iframe>
+					<div className="text-left">
+						<div className="flex flex-row space-x-4 mx-4 pb-4 md:pb-7 md:pt-10 pt-0">
+							<FaMap className="md:w-10 w-10 md:h-10 h-10" />
+							<p>
+								Jl. Babarsari No.43, Janti, Caturtunggal, Kec. Depok, Kabupaten
+								Sleman, Daerah Istimewa Yogyakarta 55281
+							</p>
+						</div>
+						<div className="flex flex-row space-x-4 mx-4 pb-4 md:pb-7">
+							<FaPhone className="md:w-8 w-5 md:h-11 h-8 " />
+							<p className="pt-2">(0274) 487711</p>
+						</div>
+						<div className="flex flex-row space-x-4 mx-4 pb-4 md:pb-7">
+							<FaEnvelope className="md:w-8 w-5 md:h-8 h-5" />
+							<p>releaffashion@gmail.com</p>
+						</div>
+						<div className="flex flex-row space-x-4 mx-4 pb-4 md:pb-7">
+							<FaFacebook className="md:w-8 w-5 md:h-8 h-5" />
+							<p>releaffashion._</p>
+						</div>
+						<div className="flex flex-row space-x-4 mx-4 pb-4 md:pb-7">
+							<FaInstagram className="md:w-8 w-5 md:h-8 h-5" />
+							<p>releaffashion._</p>
+						</div>
+						<div className="flex flex-row space-x-4 mx-4 pb-4 md:pb-10">
+							<FaTiktok className="md:w-8 w-5 md:h-8 h-5" />
+							<p>releaffashion._</p>
+						</div>
 					</div>
-					<div className="flex flex-row space-x-4 mx-4 pb-4">
-					<FaPhone className="md:w-11 w-5 md:h-11 h-5 " />
-					<p>(0274) 487711</p>
-					</div>
-					<div className="flex flex-row space-x-4 mx-4 pb-4">
-					<FaEnvelope className="md:w-11 w-5 md:h-11 h-5" />
-					<p>releaffashion@gmail.com</p>
-					</div>
-					<div className="flex flex-row space-x-4 mx-4 pb-4">
-					<FaFacebook className="md:w-11 w-5 md:h-11 h-5" />
-					<p>releaffashion._</p>
-					</div>
-					<div className="flex flex-row space-x-4 mx-4 pb-4">
-					<FaInstagram className="md:w-11 w-5 md:h-11 h-5" />
-					<p>releaffashion._</p>
-					</div>
-					<div className="flex flex-row space-x-4 mx-4 pb-4">
-					<FaTiktok className="md:w-11 w-5 md:h-11 h-5" />
-					<p>releaffashion._</p>
-					</div>
-				</div>
 				</div>
 			</section>
 		</>
