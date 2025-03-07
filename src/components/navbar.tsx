e0e0f209d44f58f475f267298ffac0ab373f69d1
StaryOne <stanyslausharyone@gmail.com>
2025-03-07 13:16:09 +0700

    fix: memperbaiki navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,218 +4,202 @@ import { FaCaretDown, FaBars, FaXmark } from "react-icons/fa6";
 import { Link } from "@tanstack/react-router";
 
 type DropdownProps = {
-    title: string;
-    items: { label: string; path: string }[];
-    isActive: boolean;
-    onClick: () => void;
+  title: string;
+  items: { label: string; path: string }[];
+  isActive: boolean;
+  onClick: () => void;
 };
 
 const Dropdown: React.FC<DropdownProps> = ({
-    title,
-    items,
-    isActive,
-    onClick,
+  title,
+  items,
+  isActive,
+  onClick,
 }) => {
-    return (
-        <div className="relative">
-            <button
-                onClick={onClick}
-                className="text-gray-700 hover:text-green-600 flex items-center"
+  return (
+    <div className="relative">
+      <button
+        onClick={onClick}
+        className="text-gray-700 hover:text-green-600 flex items-center"
+      >
+        {title} <FaCaretDown />
+      </button>
+      {isActive && (
+        <ul className="absolute left-0 mt-2 w-48 bg-white rounded-lg shadow-lg z-50">
+          {items.map((item, index) => (
+            <li
+              key={index}
+              className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
             >
-                {title} <FaCaretDown />
-            </button>
-            {isActive && (
-                <ul className="absolute left-0 mt-2 w-48 bg-white rounded-lg shadow-lg z-50">
-                    {items.map((item, index) => (
-                        <li
-                            key={index}
-                            className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                        >
-                            <Link to={item.path}>{item.label}</Link>
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </div>
-    );
+              <Link to={item.path}>{item.label}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 const Navbar: React.FC = () => {
-    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-    const [activeDropdown, setActiveDropdown] = useState<string | null>(null);
-    const navbarRef = useRef<HTMLDivElement>(null);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [activeDropdown, setActiveDropdown] = useState<string | null>(null);
+  const navbarRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        const handleClickOutside = (event: MouseEvent) => {
-            if (
-                navbarRef.current &&
-                !navbarRef.current.contains(event.target as Node)
-            ) {
-                setIsMobileMenuOpen(false);
-                setActiveDropdown(null);
-            }
-        };
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, []);
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        navbarRef.current &&
+        !navbarRef.current.contains(event.target as Node)
+      ) {
+        setIsMobileMenuOpen(false);
+        setActiveDropdown(null);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
-    return (
-        <nav
-            ref={navbarRef}
-            className="fixed top-0 left-0 w-full p-4 md:p-6 md:py-4 z-50 bg-transparent text-black"
-        >
-            <div className="flex justify-between items-center max-w-screen-xl mx-auto flex-wrap">
-                <Link to="/">
-                    <div className="flex items-center">
-                        <img src={icon} alt="icon" className="h-10 w-10 mr-2" />
-                        <h1 className="text-xl font-bold text-[#435151]">
-                            ReLeaf Fashion
-                        </h1>
-                    </div>
-                </Link>
+  return (
+    <nav
+      ref={navbarRef}
+      className="fixed top-0 left-0 w-full p-4 md:p-6 md:py-4 z-50 bg-transparent text-black"
+    >
+      <div className="flex justify-between items-center max-w-screen lg:px-23 mx-auto flex-wrap">
+        <Link to="/">
+          <div className="flex items-center">
+            <img src={icon} alt="icon" className="h-10 w-10 mr-2" />
+            <h1 className="text-xl font-bold text-[#435151]">ReLeaf Fashion</h1>
+          </div>
+        </Link>
 
-                <div className="md:hidden">
-                    <button
-                        onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-                    >
-                        {isMobileMenuOpen ? (
-                            <FaXmark className="text-black" />
-                        ) : (
-                            <FaBars className="text-black" />
-                        )}
-                    </button>
-                </div>
-
-                <div className="hidden md:flex space-x-6">
-                    <Dropdown
-                        title="Marketplace"
-                        items={[
-                            {
-                                label: "Overview",
-                                path: "/marketplace",
-                            },
-                            {
-                                label: "Shop Preloved Fashion",
-                                path: "/view",
-                            },
-                            {
-                                label: "Upload Preloved Fashion",
-                                path: "/upload",
-                            },
-                        ]}
-                        isActive={activeDropdown === "Marketplace"}
-                        onClick={() =>
-                            setActiveDropdown(
-                                activeDropdown === "Marketplace"
-                                    ? null
-                                    : "Marketplace"
-                            )
-                        }
-                    />
-                    <Dropdown
-                        title="Learn & Act"
-                        items={[
-                            { label: "Learn", path: "/learn" },
-                            { label: "Donation", path: "/donation" },
-                        ]}
-                        isActive={activeDropdown === "Learn & Act"}
-                        onClick={() =>
-                            setActiveDropdown(
-                                activeDropdown === "Learn & Act"
-                                    ? null
-                                    : "Learn & Act"
-                            )
-                        }
-                    />
-                    <Dropdown
-                        title="Community"
-                        items={[
-                            {
-                                label: "About Us",
-                                path: "/community#startLearning",
-                            },
-                            { label: "Forum Discussions", path: "/forum" },
-                            { label: "Terms & Conditions", path: "/term" },
-                        ]}
-                        isActive={activeDropdown === "Community"}
-                        onClick={() =>
-                            setActiveDropdown(
-                                activeDropdown === "Community"
-                                    ? null
-                                    : "Community"
-                            )
-                        }
-                    />
-                </div>
-            </div>
-
-            {isMobileMenuOpen && (
-                <div className="md:hidden bg-white shadow-md mt-2 p-4 space-y-4 w-full">
-                    <Dropdown
-                        title="Marketplace"
-                        items={[
-                            {
-                                label: "Overview",
-                                path: "/marketplace",
-                            },
-                            {
-                                label: "Shop Preloved Fashion",
-                                path: "/view",
-                            },
-                            {
-                                label: "Upload Preloved Fashion",
-                                path: "/upload",
-                            },
-                        ]}
-                        isActive={activeDropdown === "Marketplace"}
-                        onClick={() =>
-                            setActiveDropdown(
-                                activeDropdown === "Marketplace"
-                                    ? null
-                                    : "Marketplace"
-                            )
-                        }
-                    />
-                    <Dropdown
-                        title="Learn & Act"
-                        items={[
-                            { label: "Learn", path: "/learn" },
-                            { label: "Donation", path: "/donation" },
-                        ]}
-                        isActive={activeDropdown === "Learn & Act"}
-                        onClick={() =>
-                            setActiveDropdown(
-                                activeDropdown === "Learn & Act"
-                                    ? null
-                                    : "Learn & Act"
-                            )
-                        }
-                    />
-                    <Dropdown
-                        title="Community"
-                        items={[
-                            {
-                                label: "About Us",
-                                path: "/community#startLearning",
-                            },
-                            { label: "Forum Discussions", path: "/forum" },
-                            { label: "Terms & Conditions", path: "/term" },
-                        ]}
-                        isActive={activeDropdown === "Community"}
-                        onClick={() =>
-                            setActiveDropdown(
-                                activeDropdown === "Community"
-                                    ? null
-                                    : "Community"
-                            )
-                        }
-                    />
-                </div>
+        <div className="md:hidden">
+          <button onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
+            {isMobileMenuOpen ? (
+              <FaXmark className="text-black" />
+            ) : (
+              <FaBars className="text-black" />
             )}
-        </nav>
-    );
+          </button>
+        </div>
+
+        <div className="hidden md:flex space-x-6">
+          <Dropdown
+            title="Marketplace"
+            items={[
+              {
+                label: "Overview",
+                path: "/marketplace",
+              },
+              {
+                label: "Shop Preloved Fashion",
+                path: "/view",
+              },
+              {
+                label: "Upload Preloved Fashion",
+                path: "/upload",
+              },
+            ]}
+            isActive={activeDropdown === "Marketplace"}
+            onClick={() =>
+              setActiveDropdown(
+                activeDropdown === "Marketplace" ? null : "Marketplace"
+              )
+            }
+          />
+          <Dropdown
+            title="Learn & Act"
+            items={[
+              { label: "Learn", path: "/learn" },
+              { label: "Donation", path: "/donation" },
+            ]}
+            isActive={activeDropdown === "Learn & Act"}
+            onClick={() =>
+              setActiveDropdown(
+                activeDropdown === "Learn & Act" ? null : "Learn & Act"
+              )
+            }
+          />
+          <Dropdown
+            title="Community"
+            items={[
+              {
+                label: "About Us",
+                path: "/community#startLearning",
+              },
+              { label: "Forum Discussions", path: "/forum" },
+              { label: "Terms & Conditions", path: "/term" },
+            ]}
+            isActive={activeDropdown === "Community"}
+            onClick={() =>
+              setActiveDropdown(
+                activeDropdown === "Community" ? null : "Community"
+              )
+            }
+          />
+        </div>
+      </div>
+
+      {isMobileMenuOpen && (
+        <div className="md:hidden bg-white shadow-md mt-2 p-4 space-y-4 w-full">
+          <Dropdown
+            title="Marketplace"
+            items={[
+              {
+                label: "Overview",
+                path: "/marketplace",
+              },
+              {
+                label: "Shop Preloved Fashion",
+                path: "/view",
+              },
+              {
+                label: "Upload Preloved Fashion",
+                path: "/upload",
+              },
+            ]}
+            isActive={activeDropdown === "Marketplace"}
+            onClick={() =>
+              setActiveDropdown(
+                activeDropdown === "Marketplace" ? null : "Marketplace"
+              )
+            }
+          />
+          <Dropdown
+            title="Learn & Act"
+            items={[
+              { label: "Learn", path: "/learn" },
+              { label: "Donation", path: "/donation" },
+            ]}
+            isActive={activeDropdown === "Learn & Act"}
+            onClick={() =>
+              setActiveDropdown(
+                activeDropdown === "Learn & Act" ? null : "Learn & Act"
+              )
+            }
+          />
+          <Dropdown
+            title="Community"
+            items={[
+              {
+                label: "About Us",
+                path: "/community#startLearning",
+              },
+              { label: "Forum Discussions", path: "/forum" },
+              { label: "Terms & Conditions", path: "/term" },
+            ]}
+            isActive={activeDropdown === "Community"}
+            onClick={() =>
+              setActiveDropdown(
+                activeDropdown === "Community" ? null : "Community"
+              )
+            }
+          />
+        </div>
+      )}
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/routes/view.tsx
+++ src/routes/view.tsx
@@ -9,193 +9,193 @@ import fashion5 from "../assets/fashion5.png";
 import fashion6 from "../assets/fashion6.png";
 
 const products = [
-	{
-		id: 1,
-		name: "Colorful Clothes",
-		price: 25000,
-		image: fashion1,
-	},
-	{
-		id: 2,
-		name: "One Set Green Dress",
-		price: 25000,
-		image: fashion2,
-	},
-	{
-		id: 3,
-		name: "Bottom pink woman",
-		price: 25000,
-		image: fashion3,
-	},
-	{ id: 4, name: "Tracksuits", price: 25000, image: fashion4 },
-	{
-		id: 5,
-		name: "Cute one set clothes",
-		price: 25000,
-		image: fashion5,
-	},
-	{
-		id: 6,
-		name: "Bottom Purple Sports",
-		price: 25000,
-		image: fashion6,
-	},
+  {
+    id: 1,
+    name: "Colorful Clothes",
+    price: 25000,
+    image: fashion1,
+  },
+  {
+    id: 2,
+    name: "One Set Green Dress",
+    price: 25000,
+    image: fashion2,
+  },
+  {
+    id: 3,
+    name: "Bottom pink woman",
+    price: 25000,
+    image: fashion3,
+  },
+  { id: 4, name: "Tracksuits", price: 25000, image: fashion4 },
+  {
+    id: 5,
+    name: "Cute one set clothes",
+    price: 25000,
+    image: fashion5,
+  },
+  {
+    id: 6,
+    name: "Bottom Purple Sports",
+    price: 25000,
+    image: fashion6,
+  },
 ];
 
 export const Route = createFileRoute("/view")({
-	component: ViewComponent,
+  component: ViewComponent,
 });
 
 function ViewComponent() {
-	const [search, setSearch] = useState("");
-	return (
-		<>
-			<div className="min-h-full bg-white text-black">
-				<div className="mt-17   mx-7 md:mx-32  max-sm:mt-17">
-					<ol className="inline-flex items-center space-x-1 md:space-x-3">
-						<li className="inline-flex items-center">
-							<a
-								href="/"
-								className="inline-flex items-center text-sm font-medium text-gray-500 hover:text-green-300"
-							>
-								Home
-							</a>
-						</li>
-						<li>
-							<div className="flex items-center">
-								<svg
-									className="w-6 h-6 text-gray-400"
-									fill="currentColor"
-									viewBox="0 0 20 20"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										fill-rule="evenodd"
-										d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-										clip-rule="evenodd"
-									></path>
-								</svg>
-								<a
-									href="/marketplace"
-									className="ml-1 text-sm font-medium text-gray-500 hover:text-green-300 md:ml-2"
-								>
-									Marketplace
-								</a>
-							</div>
-						</li>
-						<li>
-							<div className="flex items-center">
-								<svg
-									className="w-6 h-6 text-gray-400"
-									fill="currentColor"
-									viewBox="0 0 20 20"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										fill-rule="evenodd"
-										d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-										clip-rule="evenodd"
-									></path>
-								</svg>
-								<span className="ml-1 text-sm font-medium text-gray-500 md:ml-2">
-									View
-								</span>
-							</div>
-						</li>
-					</ol>
-				</div>
-				<div className="w-full flex flex-col items-start pt-5 md:pt-5 md:flex-row gap-6 p-6 md:ps-32 md:pe-32">
-					<div className="w-full md:h-full md:w-3/9 bg-white p-4 rounded-lg border border-black">
-						<h2 className="text-center font-bold text-lg mb-4">Filters</h2>
-						<hr />
-						<div className="mb-4 mt-4">
-							<div className="flex items-center justify-between">
-								<span>Tops</span>
-								<FaAngleRight style={{ color: "gray" }} />
-							</div>
-							<div className="flex items-center justify-between">
-								<span>Bottoms</span>
-								<FaAngleRight style={{ color: "gray" }} />
-							</div>
-							<div className="flex items-center justify-between">
-								<span>One Set</span>
-								<FaAngleRight style={{ color: "gray" }} />
-							</div>
-						</div>
-						<hr />
-						<div className="mb-4">
-							<h3 className="font-medium my-2">Price</h3>
-							<div>
-								<input type="radio" name="price" id="low" className="mr-2" />
-								<label htmlFor="low">Up to Rp 50.000</label>
-							</div>
-							<div>
-								<input type="radio" name="price" id="mid" className="mr-2" />
-								<label htmlFor="mid">Rp 50.000 - Rp 100.000</label>
-							</div>
-							<div>
-								<input type="radio" name="price" id="high" className="mr-2" />
-								<label htmlFor="high">Above Rp 100.000</label>
-							</div>
-						</div>
-						<hr />
-						<div>
-							<h3 className="font-medium my-2">Size</h3>
-							<div className="flex flex-wrap gap-y-5 gap-x-5 pb-2">
-								<button className="bg-[#FFD8BE] rounded-full w-[30%]">
-									Small
-								</button>
-								<button className="bg-gray-100 rounded-full w-[30%]">
-									Medium
-								</button>
-								<button className="bg-gray-100 rounded-full w-[30%]">
-									Large
-								</button>
-								<button className="bg-gray-100 rounded-full w-[30%]">
-									Large-X
-								</button>
-							</div>
-						</div>
-						<hr />
-						<button className="bg-[#FFD8BE] px-4 py-2 rounded-full w-full mt-4">
-							Apply
-						</button>
-					</div>
+  const [search, setSearch] = useState("");
+  return (
+    <>
+      <div className="min-h-full bg-white text-black">
+        <div className="mt-17   mx-7 md:mx-32  max-sm:mt-1">
+          <ol className="inline-flex items-center space-x-1 md:space-x-3">
+            <li className="inline-flex items-center">
+              <a
+                href="/"
+                className="inline-flex items-center text-sm font-medium text-gray-500 hover:text-green-300"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <div className="flex items-center">
+                <svg
+                  className="w-6 h-6 text-gray-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <a
+                  href="/marketplace"
+                  className="ml-1 text-sm font-medium text-gray-500 hover:text-green-300 md:ml-2"
+                >
+                  Marketplace
+                </a>
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center">
+                <svg
+                  className="w-6 h-6 text-gray-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2">
+                  View
+                </span>
+              </div>
+            </li>
+          </ol>
+        </div>
+        <div className="w-full flex flex-col items-start pt-5 md:pt-5 md:flex-row gap-6 p-6 md:ps-32 md:pe-32">
+          <div className="w-full md:h-full md:w-3/9 bg-white p-4 rounded-lg border border-black">
+            <h2 className="text-center font-bold text-lg mb-4">Filters</h2>
+            <hr />
+            <div className="mb-4 mt-4">
+              <div className="flex items-center justify-between">
+                <span>Tops</span>
+                <FaAngleRight style={{ color: "gray" }} />
+              </div>
+              <div className="flex items-center justify-between">
+                <span>Bottoms</span>
+                <FaAngleRight style={{ color: "gray" }} />
+              </div>
+              <div className="flex items-center justify-between">
+                <span>One Set</span>
+                <FaAngleRight style={{ color: "gray" }} />
+              </div>
+            </div>
+            <hr />
+            <div className="mb-4">
+              <h3 className="font-medium my-2">Price</h3>
+              <div>
+                <input type="radio" name="price" id="low" className="mr-2" />
+                <label htmlFor="low">Up to Rp 50.000</label>
+              </div>
+              <div>
+                <input type="radio" name="price" id="mid" className="mr-2" />
+                <label htmlFor="mid">Rp 50.000 - Rp 100.000</label>
+              </div>
+              <div>
+                <input type="radio" name="price" id="high" className="mr-2" />
+                <label htmlFor="high">Above Rp 100.000</label>
+              </div>
+            </div>
+            <hr />
+            <div>
+              <h3 className="font-medium my-2">Size</h3>
+              <div className="flex flex-wrap gap-y-5 gap-x-5 pb-2">
+                <button className="bg-[#FFD8BE] rounded-full w-[30%]">
+                  Small
+                </button>
+                <button className="bg-gray-100 rounded-full w-[30%]">
+                  Medium
+                </button>
+                <button className="bg-gray-100 rounded-full w-[30%]">
+                  Large
+                </button>
+                <button className="bg-gray-100 rounded-full w-[30%]">
+                  Large-X
+                </button>
+              </div>
+            </div>
+            <hr />
+            <button className="bg-[#FFD8BE] px-4 py-2 rounded-full w-full mt-4">
+              Apply
+            </button>
+          </div>
 
-					<div className="w-full h-full flex flex-col">
-						<input
-							type="text"
-							placeholder="Search Products"
-							className="w-full mb-4 border rounded-full px-4 py-2 w-80 border-none outline-none bg-gray-100"
-							value={search}
-							onChange={(e) => setSearch(e.target.value)}
-						/>
-						<a href="/product" className="cursor-pointer">
-							<div className="w-full md:w-full grid grid-cols-1 md:grid-cols-3 gap-6">
-								{products
-									.filter((p) =>
-										p.name.toLowerCase().includes(search.toLowerCase())
-									)
-									.map((product) => (
-										<div key={product.id}>
-											<div className="flex justfiy-center items-center aspect-square bg-gray-100 p-4 rounded-lg">
-												<img
-													src={product.image}
-													alt={product.name}
-													className="w-full h-full h-40 object-cover mb-2"
-												/>
-											</div>
-											<h3 className="text-md font-normal">{product.name}</h3>
-											<p className="font-bold">
-												Rp {product.price.toLocaleString()}
-											</p>
-										</div>
-									))}
-							</div>
-						</a>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+          <div className="w-full h-full flex flex-col">
+            <input
+              type="text"
+              placeholder="Search Products"
+              className="w-full mb-4 border rounded-full px-4 py-2 w-80 border-none outline-none bg-gray-100"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <a href="/product" className="cursor-pointer">
+              <div className="w-full md:w-full grid grid-cols-1 md:grid-cols-3 gap-6">
+                {products
+                  .filter((p) =>
+                    p.name.toLowerCase().includes(search.toLowerCase())
+                  )
+                  .map((product) => (
+                    <div key={product.id}>
+                      <div className="flex justfiy-center items-center aspect-square bg-gray-100 p-4 rounded-lg">
+                        <img
+                          src={product.image}
+                          alt={product.name}
+                          className="w-full h-full h-40 object-cover mb-2"
+                        />
+                      </div>
+                      <h3 className="text-md font-normal">{product.name}</h3>
+                      <p className="font-bold">
+                        Rp {product.price.toLocaleString()}
+                      </p>
+                    </div>
+                  ))}
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
